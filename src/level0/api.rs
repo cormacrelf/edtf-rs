fcc89a645031d6d03d40be35cb5df00a23647bdc
Cormac Relf <web@cormacrelf.net>
2021-06-01 16:09:04 +1000

    cargo.toml fields + MPL-2.0 license file

--- src/level0/api.rs
+++ src/level0/api.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright © 2021 Corporation for Digital Scholarship
+
 //! # EDTF Level 0
 //!
 //! [EDTF Specification](https://www.loc.gov/standards/datetime/), February 4, 2019
--- src/level1/mod.rs
+++ src/level1/mod.rs
@@ -1,3 +1,9 @@
+// This Source Code Form is subject to the terms of the Mozilla Public
+// License, v. 2.0. If a copy of the MPL was not distributed with this
+// file, You can obtain one at http://mozilla.org/MPL/2.0/.
+//
+// Copyright © 2021 Corporation for Digital Scholarship
+
 use core::convert::TryInto;
 use crate::common::is_valid_complete_date;
 use crate::ParseError;
